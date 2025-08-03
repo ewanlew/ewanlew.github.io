658bcd5c0858b2d5ea05735ffe7b6263e06fd28f
ewanlew <97612083+ewanlew@users.noreply.github.com>
2025-08-03 16:24:27 +0100

    updated 404

--- src/NotFound.jsx
+++ src/NotFound.jsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import { Link } from "react-router-dom";
 import Background from "./Background";
 import CustomCursor from "./CustomCursor";
 
@@ -12,9 +13,14 @@ function NotFound() {
             <Background />
             <CustomCursor />
             <div className="page-card">
-                <h1>404 - Page Not Found</h1>
-                <p>uh oh - page doesn't exist :(</p>
-                <p><a href="/">Home</a></p>
+                
+                <h1>404 - page not found</h1>
+                <p>the page you're looking for doesn't exist :(</p>
+                <p>if you feel that this page should exist, please <Link to="/contact">contact me!</Link></p>
+
+                <div className="nav-buttons">
+                    <Link to="/" tabIndex={1} className="nav-button">go home!</Link>
+                </div>
             </div>
         </div>
     );
